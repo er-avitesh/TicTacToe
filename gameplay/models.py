3e88151a645dcd1c5869d1b380be8a9b850a5f4f
Kesharwani <avitesh.kesharwani@capgemini.com>
2019-12-30 00:00:35 -0500

    project commit

---
+++ gameplay/models.py
@@ -0,0 +1,44 @@
+from django.contrib.auth.models import User
+from django.db import models
+from django.db.models import Q
+
+GAME_STATUS_CHOICES = (
+    ('F', 'First Player Chance'),
+    ('S', 'Second Player Chance'),
+    ('W', 'First Player WINS'),
+    ('L', 'Second Player WINS'),
+    ('D', 'Draw')
+)
+
+
+class GameQuerySet(models.QuerySet):
+    def games_for_user(self, user):
+        return self.filter(
+            Q(first_player=user) | Q(second_player=user)
+        )
+
+    def active_games(self):
+        return self.filter(
+            Q(status='F') | Q(status='S')
+        )
+
+
+class Game(models.Model):
+    first_player = models.ForeignKey(User, related_name="game_first_player", on_delete=models.CASCADE)
+    second_player = models.ForeignKey(User, related_name="game_second_player", on_delete=models.CASCADE)
+    start_time = models.DateTimeField(auto_now_add=True)
+    last_active = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=1, default='F', choices=GAME_STATUS_CHOICES)
+
+    objects = GameQuerySet.as_manager()
+
+    def __str__(self):
+        return "{0} vs {1}".format(self.first_player, self.second_player)
+
+
+class Move(models.Model):
+    x = models.IntegerField()
+    y = models.IntegerField()
+    comment = models.CharField(max_length=300, blank=True)
+    by_first_player = models.BooleanField()
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
---
+++ player/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+from gameplay.models import Game
+
+
+def home(request):
+    my_games = Game.objects.games_for_user(request.user)
+    active_games = my_games.active_games()
+    return render(request, "player/home.html", {'games': active_games})
+#    games_first_player = Game.objects.filter(
+#        first_player=request.user,
+#        status='F'
+#    )
+#    games_second_player = Game.objects.filter(
+#        second_player=request.user,
+#        status='S'
+#    )
+#    all_my_games = list(games_first_player) + \
+#                   list(games_second_player)
+#    return render(request, "player/home.html", {'games': all_my_games})
+
